340d0781cdd66554a0ceab73ece6a8e23e80659c
kevinmeyvaert <kevin.meyvaert@gmail.com>
2024-11-04 18:10:07 +0100

    fix: use import for logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,7 @@
 import { Link } from "@remix-run/react";
 
+import logo from "../images/logo.svg";
+
 export const Header = () => {
   return (
     <header className="container z-20 p-[20px_40px] flex justify-start items-center h-[150px]">
@@ -10,7 +12,7 @@ export const Header = () => {
         }}
       >
         <div className="flex items-center">
-          <img src="../public/logo.svg" width="80" alt="Behangmotief"></img>
+          <img src={logo} width="80" alt="Behangmotief"></img>
           <p className="ml-2">| concert- & festivalphotographer</p>
         </div>
       </Link>
